bae06d4c1793eea8d9838029b1bb7d88c6767590
Jekel8888 <52115322+Jekel8888@users.noreply.github.com>
2019-12-03 10:39:39 -0700

    testing parseFloat method with sorting

--- server.js
+++ server.js
@@ -1,21 +1,35 @@
-const express = require('express')
-const morgan = require('morgan')
-const playstore = require('./playstore')
+// sort the list by either rating or app, any other value results in an error,
+//  if no value provided do not perform a sort.
 
-const app = express()
+const express = require("express");
+const morgan = require("morgan");
+const playstore = require("./playstore");
 
-app.use(morgan('dev'))
+const app = express();
 
-app.get('/apps', (req,res) => {
-    const genre = req.query.genres;
+app.use(morgan("dev"));
 
-    let playStoreCopy = [...playstore]
-    
-    
-    playStoreCopy = playStoreCopy.filter(game => game.Genres.toLowerCase().includes(genre.toLowerCase()))
+app.get("/apps", (req, res) => {
+  const genre = req.query.genres;
+  const sort = req.query.sort;
+  const validSortValues = ["rating", "app"];
+  if (sort && !validSortValues.includes(sort)) {
+    return res
+      .status(400)
+      .json({ message: 'Sort field must be one of "rating" or "app"' });
+  }
+  let playStoreCopy = [...playstore];
 
-    
-    res.json(playStoreCopy)
-})
+  if (sort) {
+    playStoreCopy.sort((a, b) => (a[sort] < b[sort] ? -1 : 1));
+  }
 
-app.listen(8000, () => console.log('listening on 8000'))
+  if (genre) {
+    playStoreCopy = playStoreCopy.filter(game =>
+      game.Genres.toLowerCase().includes(genre.toLowerCase())
+    );
+  }
+  res.json(playStoreCopy);
+});
+
+app.listen(8000, () => console.log("listening on 8000"));
